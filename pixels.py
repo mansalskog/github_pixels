8e7860859b32797a21f54bcfc25633ba9c0c0bf4
Måns Alskog <manal270@student.liu.se>
2020-10-03 17:11:59 +0200

    Better usability

--- pixels.py
+++ pixels.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python3
 
-import subprocess
+import sys
+import os
 import datetime
-import math
+import subprocess
+import tempfile
 
-FILE_NAME = "timestamp.txt"
-PIXELS = """
+if len(sys.argv) < 4:
+    print("usage: " + sys.argv[0] + "<repo_url> <author_name> <author_email>")
+    exit()
+
+repo_url = sys.argv[1]
+author_name = sys.argv[2]
+author_email = sys.argv[3]
+
+file_name = "dummy.txt"
+
+def commit(date, content):
+    with open(file_name, "w") as f:
+        f.write(content)
+
+    git_env = {
+        "GIT_AUTHOR_NAME": author_name,
+        "GIT_COMMITTER_NAME": author_name,
+        "GIT_AUTHOR_EMAIL": author_email,
+        "GIT_COMMITTER_EMAIL": author_email,
+        "GIT_AUTHOR_DATE": date.isoformat(),
+        "GIT_COMMITTER_DATE": date.isoformat()}
+    subprocess.run(
+        ["git", "commit", "-m", content, "--", file_name],
+        env = git_env)
+
+pixels = """
 ###  ##       #   #       ##### #                  #
   #  # #     # # # #     #  #   #                  #
   #  # #  #  # # # #     #  #   #      #  # #   #  #
@@ -13,32 +39,30 @@ PIXELS = """
 # #  #  ###  #   #  ## #    # # #  # ###  #   ###  #
 # #  # # #  ##  ##  #  #    #   #  ## #  ##    #    
  ##  ##   ##  ##  ## ##     #   #  #   ## #     ## #"""
-PIXELS = PIXELS.replace("\n", "")
-if len(PIXELS) != 52 * 7:
-    print("Invalid pixels!")
+pixels = pixels.replace("\n", "")
+if len(pixels) != 52 * 7:
+    print("pixels should be 52 * 7 array")
     exit()
 
-def commit(date, content):
-    with open(FILE_NAME, "w") as f:
-        f.write(content)
-    git_env = {
-        "GIT_AUTHOR_DATE": date.isoformat(),
-        "GIT_COMMITTER_DATE": date.isoformat()}
-    subprocess.run(
-        ["git", "commit", "-m", "'" + content + "'", "--", FILE_NAME],
-        env = git_env)
+with tempfile.TemporaryDirectory() as tmp_dir:
+    os.chdir(tmp_dir)
+    subprocess.run(["git", "clone", repo_url, "."])
+    open(file_name, "w").close()
+    subprocess.run(["git", "add", file_name])
 
+    today = datetime.datetime.combine(
+        datetime.date.today(),
+        datetime.time(13, 37))
+    end_of_week = today + datetime.timedelta(
+        days = (5 - today.weekday()) % 7)
 
-today = datetime.datetime.combine(
-    datetime.date.today(),
-    datetime.time(13, 37))
-end_of_week = today + datetime.timedelta(
-    days = (5 - today.weekday()) % 7)
+    commit_date = end_of_week - datetime.timedelta(days = len(pixels) - 1)
+    for i in range(len(pixels)):
+        j = 52 * (i % 7) + i // 7
+        if pixels[j] != " ":
+            commit(commit_date, str(i))
 
-commit_date = end_of_week - datetime.timedelta(days = len(PIXELS) - 1)
+        commit_date += datetime.timedelta(days = 1)
 
-for i in range(len(PIXELS)):
-    j = 52 * (i % 7) + i // 7
-    if PIXELS[j] != " ":
-        commit(commit_date, str(i))
-    commit_date += datetime.timedelta(days = 1)
+    print("input github credentials to push commits")
+    subprocess.run(["git", "push", "origin", "master"])
